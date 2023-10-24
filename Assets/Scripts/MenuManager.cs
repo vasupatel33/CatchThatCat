da63559f499608d95eaa12908e41b4d0fc4996f1
vasupatel <vasurabadiya5@gmail.com>
2023-10-24 23:18:37 +0530

    Done

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{ 
+    public void PlayBTn()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
